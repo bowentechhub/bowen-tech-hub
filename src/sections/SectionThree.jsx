1d69a9f13b5ed3e0fdf7c9f6b0d3e08b6c55ae40
boluamoo <boluamoo58@gmail.com>
2025-02-21 12:14:13 +0100

    refactor SectionThree component to improve image handling and add hover effects

--- src/sections/SectionThree.jsx
+++ src/sections/SectionThree.jsx
@@ -4,7 +4,7 @@ import about2 from "../assets/images/about2.jpeg";
 
 const SectionThree = () => {
   return (
-    <div className="px-[5%] py-[100px] lg:py-[180px] flex flex-col" id='about'>
+    <div className="px-[5%] py-[100px] lg:py-[180px] flex flex-col" id="about">
       <div className="flex flex-col">
         <p className="font-semibold text-xl lg:text-[28px] leading-[66px]">
           Who We Are
@@ -19,22 +19,30 @@ const SectionThree = () => {
       </div>
       <div className="flex flex-col lg:flex-row gap-[21px] max-h-min">
         <div className="flex flex-row lg:flex-col lg:gap-2 lg:w-1/2 lg:flex-1">
-          <img
-            src={about1}
-            className="border-[4px] lg:border-[10px] border-white w-1/2 lg:w-full max-h-[477px] object-cover"
+          <div
+            className="border-[4px] lg:border-[10px] border-white w-1/2 lg:w-full max-h-[477px] overflow-hidden"
             style={{
               boxShadow:
                 "2px 2px 24.2px 0px #0000000A, -1px 0px 26px 0px #0000000A",
             }}
-          />
-          <img
-            src={about2}
-            className="border-[4px] lg:border-[10px] border-white w-1/2 lg:w-full max-h-[300px] object-cover"
+          >
+            <img
+              src={about1}
+              className="size-full object-cover hover:scale-[1.2] transition-all duration-300"
+            />
+          </div>
+          <div
+            className="border-[4px] lg:border-[10px] border-white w-1/2 lg:w-full max-h-[300px] overflow-hidden"
             style={{
               boxShadow:
                 "2px 2px 24.2px 0px #0000000A, -1px 0px 26px 0px #0000000A",
             }}
-          />
+          >
+            <img
+              src={about2}
+              className="size-full object-cover hover:scale-[1.2] transition-all duration-300"
+            />
+          </div>
         </div>
         <div className="flex flex-col gap-[43px] lg:gap-[86px] w-full lg:w-1/2 lg:flex-1">
           <div>
@@ -48,14 +56,18 @@ const SectionThree = () => {
               you embark on this transformative learning experience.
             </p>
           </div>
-          <img
-            src={learn}
-            className="border-[10px] border-white size-full object-cover"
+          <div
+            className="border-[10px] border-white size-full object-cover overflow-hidden"
             style={{
               boxShadow:
                 "2px 2px 24.2px 0px #0000000A, -1px 0px 26px 0px #0000000A",
             }}
-          />
+          >
+            <img
+              src={learn}
+              className="size-full object-cover hover:scale-[1.2] transition-all duration-300"
+            />
+          </div>
         </div>
       </div>
     </div>
